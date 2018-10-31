e60d942a3b942108e2096214495ad2513717bac3
zhaohanqing95 <zhaohanqingss@163.com>
2018-10-31 15:57:35 +0900

    181031_solved

--- src/dc_181031/Solution.java
+++ src/dc_181031/Solution.java
@@ -1,7 +1,15 @@
 package dc_181031;
 
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println("TEST");
+class Solution {
+    public String solution(String s) {
+        int i=s.length();
+        String answer;
+        if(i%2==0){
+            answer = s.substring((i/2-1),(i/2+1));
+        }
+        else {
+            answer = s.substring(i/2, i/2+1);
+        }
+        return answer;
     }
 }
