b5c067d3f0e4f473d3f2861fafc0ac57791b9e7e
zhaohanqing95 <zhaohanqingss@163.com>
2018-11-02 16:54:39 +0900

    add problem

--- src/dc_181031/Solution.java
+++ src/dc_181031/Solution.java
@@ -1,15 +1,16 @@
 package dc_181031;
 
 class Solution {
-    public String solution(String s) {
+
+
+    public static void main(String[] args) {
+        String s = "abcde";
         int i=s.length();
         String answer;
-        if(i%2==0){
+        if(i%2==0)
             answer = s.substring((i/2-1),(i/2+1));
-        }
-        else {
-            answer = s.substring(i/2, i/2+1);
-        }
-        return answer;
+        else
+            answer = String.valueOf(s.charAt(i/2));
+        System.out.println(answer);
     }
 }
