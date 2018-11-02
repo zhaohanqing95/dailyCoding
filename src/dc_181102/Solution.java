a593b3c5c33be0cdb047857db36342d47f18b894
zhaohanqing95 <zhaohanqingss@163.com>
2018-11-02 16:51:26 +0900

    add problem

---
+++ src/dc_181102/Solution.java
@@ -0,0 +1,22 @@
+package dc_181102;
+
+public class Solution {
+    public static void main(String[] args) {
+     int a=6;
+     int b=3;
+     int answer=0;
+     if(a < b){
+         for (int i = a; i <=b ; i++) {
+             answer=answer+i;
+         }
+     }
+     else if (a > b){
+         for (int i = b; i <=a; i++)
+             answer = answer + i;
+     }
+        else{
+         answer=a;
+     }
+        System.out.println(answer);
+    }
+}
