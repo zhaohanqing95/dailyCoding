ed1319e919e6f06cf76789ec4a0439241bc5dde3
zhaohanqing95 <zhaohanqingss@163.com>
2018-11-05 15:59:17 +0900

    add problem

---
+++ src/dc_181105/Solution.java
@@ -0,0 +1,23 @@
+package dc_181105;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr={1,5,6,7,4,7,5,15,5,17};
+        int divisior=5;
+        ArrayList<Integer> answer = new ArrayList();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] %divisior ==0)
+                answer.add(arr[i]);
+        }
+        if (answer.size() ==0)
+            System.out.println("-1");
+        else
+            Collections.sort(answer);
+            for (int i = 0; i <answer.size(); i++) {
+                System.out.println(answer.get(i));
+            }
+    }
+}
