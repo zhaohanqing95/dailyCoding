b416a29d3a9c773bf122410f4e070a41a5ee5d88
zhaohanqing95 <zhaohanqingss@163.com>
2018-11-07 11:20:13 +0900

    add problem

--- src/dc_181106/Solution.java
+++ src/dc_181106/Solution.java
@@ -6,12 +6,12 @@ public class Solution {
         int b = 31;
         int day = 0;
         int rem;
-        String[] week = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        String[] week = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
         int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
         for (int i = 0; i < a-1; i++)
             day = day + month[i];
         day = day + b;
-        rem = (day % 7) - 1;
+        rem = (day % 7) ;
         System.out.println(week[rem]);
     }
 }
