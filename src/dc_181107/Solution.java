6bc4c5a13107b9c2d17344666d8f8a8946710071
zhaohanqing95 <zhaohanqingss@163.com>
2018-11-07 17:50:28 +0900

    add problem

---
+++ src/dc_181107/Solution.java
@@ -0,0 +1,32 @@
+package dc_181107;
+
+import javafx.scene.chart.Chart;
+
+public class Solution {
+    public static void main(String[] args) {
+        boolean answer  ;
+        String s = "Pyy";
+        char[] chars =s.toCharArray();
+        char p = 'p';
+        char P = 'P';
+        char Y = 'Y';
+        char y = 'y';
+        int nump = 0;
+        int numy = 0;
+        for (int i = 0; i <s.length() ; i++) {
+            if(p == chars[i])
+                nump++;
+            if(P == chars[i])
+                nump++;
+            if(Y == chars[i])
+               numy++;
+            if(y == chars[i])
+                numy++;
+        }
+        if (nump == numy)
+            answer = true;
+        else
+            answer = false;
+        System.out.println(answer);
+    }
+}
