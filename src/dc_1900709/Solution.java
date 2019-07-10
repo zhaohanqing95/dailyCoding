076c81fd2a975e024c7079988aa9fd3a3a505f6a
zhaohanqing95 <zhaohanqingss@163.com>
2019-07-10 17:00:36 +0900

    20190710

--- src/dc_1900709/Solution.java
+++ src/dc_1900709/Solution.java
@@ -1,6 +1,7 @@
 package dc_1900709;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Solution
 {
@@ -8,7 +9,18 @@ public class Solution
         String[][] clothes ={{"yellow_hat","headgear" },{"blue_sunglasses","eyewear"},{"green_turban","headgear"}};
         int answer = 1;
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        
-        System.out.println(answer-1);
+        for (int i = 0; i < clothes.length; i++) {
+            if(map.get(clothes[i][1]) != null)
+                map.replace(clothes[i][1], map.get(clothes[i][1])+1);
+             else
+                map.put(clothes[i][1], 1);
+        }
+        Iterator iterator = map.keySet().iterator();
+        while(iterator.hasNext()){
+            String key = (String) iterator.next();
+            answer = answer * (map.get(key) + 1);
+        }
+        answer--;
+        System.out.println(answer);
     }
 }
---
+++ src/dc_190710/Solution.java
@@ -0,0 +1,19 @@
+package dc_190710;
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 15;
+        int answer = 1;
+        for (int i = 1; i < n / 2 +1; i++) {
+            int count = 0;
+                for (int k = i; count <= n; k++) {
+                    count = count + k;
+                    if (count == n){
+                        answer = answer + 1;
+                        break;
+                    }
+            }
+        }
+        System.out.println( answer);
+    }
+}
