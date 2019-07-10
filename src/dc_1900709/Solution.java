4e9e8ef441fdc0f70290ed14b8f5a8433687febf
zhaohanqing95 <zhaohanqingss@163.com>
2019-07-10 15:01:32 +0900

    190710

---
+++ src/dc_1900709/Solution.java
@@ -0,0 +1,14 @@
+package dc_1900709;
+
+import java.util.HashMap;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        String[][] clothes ={{"yellow_hat","headgear" },{"blue_sunglasses","eyewear"},{"green_turban","headgear"}};
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
+        System.out.println(answer-1);
+    }
+}
--- src/dc_190708/Solution.java
+++ src/dc_190708/Solution.java
@@ -4,37 +4,23 @@ public class Solution {
     public static void main(String[] args) {
         String s = "(()(";
         boolean answer = true;
-        int[] array_s = new int[s.length() +1 ];
+        int[] array_s = new int[s.length()];
         int count = 0;
-        array_s[s.length()] = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(')
                 array_s[i] = 1;
             else
                 array_s[i] = -1;
         }
-        for (int i = 0; i < array_s.length - 1; i++) {
+        for (int i = 0; i < array_s.length ; i++) {
             count = count + array_s[i];
-            if (count > 0) {
-                if (array_s[i] == -1 && array_s[i + 1] == -1)
-                    continue;
-                else {
-                    if (array_s[i] == 1)
-                        continue;
-                    else {
-                        answer = false;
-                        break;
-                    }
-                }
-            } else {
-                if (count == 0 && (array_s[i + 1] >= 0)) {
-                    continue;
-                } else {
+            if (count >= 0)
+                continue;
+             else {
                     answer = false;
                     break;
                 }
             }
-        }
         // count = count + array_s[array_s.length - 1];
         if (count == 0)
             answer = true;
